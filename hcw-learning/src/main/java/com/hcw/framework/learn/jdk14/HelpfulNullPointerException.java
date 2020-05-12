2d1e3344528b3d2619c5b247aa438a8718c2a920
huangchunwu <951123033@qq.com>
2020-05-12 23:08:38 +0800

    jdk14 NPE的优化

--- hcw-learning/src/main/java/com/hcw/framework/learn/jdk14/HelpfulNullPointerException.java
+++ hcw-learning/src/main/java/com/hcw/framework/learn/jdk14/HelpfulNullPointerException.java
@@ -1,7 +1,17 @@
 package com.hcw.framework.learn.jdk14;
 
+/**
+ * JEP 358增强了对NullPointerException异常的处理，可以显示详细的信息。这个功能需要通过选项-XX:+ShowCodeDetailsInExceptionMessages启用
+ */
 public class HelpfulNullPointerException {
 
+    public static void main(String[] args) {
+        //after jdk14
+        String s = null;
+        String b = "";
+        System.out.println(s.toString() + b.toString());
+    }
     
+
     
 }
