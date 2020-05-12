f5e801d94eaf85a82b86eabe27ceb2ec981d90e8
huangchunwu <951123033@qq.com>
2020-05-12 23:21:22 +0800

    jdk14 新增record新属性

--- hcw-learning/src/main/java/com/hcw/framework/learn/jdk14/Records.java
+++ hcw-learning/src/main/java/com/hcw/framework/learn/jdk14/Records.java
@@ -1,6 +1,18 @@
 package com.hcw.framework.learn.jdk14;
 
+//这个record关键字的语义，是简化bean，省略掉繁琐的构造函数、equals()、hashCode()、toString()等，如下面这种格式：
+
 public class Records {
-    
-    
+
+    public static void main(String[] args) {
+
+        //after jdk14 
+        // 定义
+        record Image(String mediaId){}
+        // 使用
+        Image image = new Image("传mediaId");
+        // 取里面的值
+        System.out.println(image.mediaId());
+    }
+
 }
