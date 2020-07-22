ee79f07fc6a0fd1b802ed2271bd15719e49bd1a0
huangchunwu <951123033@qq.com>
2020-07-22 23:26:36 +0800

    add 日志适配框架

--- hcw-learn-mylog/src/main/java/Test.java
+++ hcw-learn-mylog/src/main/java/Test.java
@@ -4,7 +4,11 @@ public class Test {
 
 
     public static void main(String[] args) {
-        LOG.info("my log is starting");
+
+        LogFactory.useLog4J2Logging();
+        LOG.info("my log is starting 1");
+        LogFactory.useSlf4jLogging();
+        LOG.info("my log is starting 2");
 
     }
 
