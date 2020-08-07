c19bab9a8a32942db5cd2ed75a0b91b806290c5e
huangchunwu <951123033@qq.com>
2020-08-07 15:03:44 +0800

    add xpath解析xml

--- hcw-learn-mybatis/src/main/java/com/hcw/learn/mybatis/parse/XPathParser.java
+++ hcw-learn-mybatis/src/main/java/com/hcw/learn/mybatis/parse/XPathParser.java
@@ -1,8 +1,14 @@
 package com.hcw.learn.mybatis.parse;
 
+import org.w3c.dom.Node;
 import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
+import javax.xml.XMLConstants;
 import javax.xml.namespace.QName;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
@@ -10,42 +16,62 @@ import javax.xml.xpath.XPathFactory;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class XPathParser {
 
 
-    private XPathFactory xPathFactory;
     private XPath xPath;
 
-    public XPathParser() {
+    private DocumentBuilder builder;
+
+    public XPathParser(){
         // 1.创建一个XPathFactory的实例
-        xPathFactory = XPathFactory.newInstance();
+        XPathFactory xPathFactory = XPathFactory.newInstance();
         // 2.通过XPathFactory的实例获取一个XPath实例
         xPath = xPathFactory.newXPath();
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        try {
+            factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+            builder = factory.newDocumentBuilder();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
     }
 
 
 
-    public String parse(String fileUrl,String expression) throws FileNotFoundException, XPathExpressionException {
-        return parse(new FileReader(fileUrl),expression);
+    public String evalString(String fileUrl,String expression) throws FileNotFoundException, XPathExpressionException {
+        return evalString(new FileReader(fileUrl),expression);
     }
 
-    public String parse(File file,String expression) throws FileNotFoundException, XPathExpressionException {
-        return parse(new FileReader(file),expression);
+    public String evalString(File file,String expression) throws FileNotFoundException, XPathExpressionException {
+        return evalString(new FileReader(file),expression);
     }
 
 
-    public String parse(FileReader fileReader,String expression) throws FileNotFoundException, XPathExpressionException{
-        return parse(new InputSource(fileReader),expression);
+    public String evalString(FileReader fileReader,String expression) throws FileNotFoundException, XPathExpressionException{
+        return evalString(new InputSource(fileReader),expression);
     }
 
-    public String parse(InputSource inputSource,String expression) throws FileNotFoundException, XPathExpressionException{
+    public String evalString(InputSource inputSource,String expression) throws FileNotFoundException, XPathExpressionException{
         //3.通过XPath实例的evaluate()方法对XML进行查询
-        String evaluate = (String) parse(inputSource,expression, XPathConstants.STRING);
+        String evaluate = (String) evaluate(inputSource,expression, XPathConstants.STRING);
         return evaluate;
     }
 
-    public Object parse(InputSource inputSource, String expression, QName qName) throws FileNotFoundException, XPathExpressionException{
+//    public Node evalNode(String fileUrl,String expression) throws IOException, SAXException, XPathExpressionException {
+//        return (Node) evaluate(builder.parse(new InputSource(new FileReader(fileUrl))),expression,XPathConstants.NODE);
+//    }
+
+    public Node evalNode(String fileUrl,String expression) throws IOException, SAXException, XPathExpressionException {
+        return (Node) evaluate(new InputSource(new FileReader(fileUrl)),expression,XPathConstants.NODE);
+    }
+
+
+    public Object evaluate(InputSource inputSource, String expression, QName qName) throws FileNotFoundException, XPathExpressionException{
         return xPath.evaluate(expression, inputSource, qName);
     }
+
 }
--- hcw-learn-mybatis/src/test/java/com/hcw/learn/mybatis/parse/XPathParserTest.java
+++ hcw-learn-mybatis/src/test/java/com/hcw/learn/mybatis/parse/XPathParserTest.java
@@ -2,9 +2,13 @@ package com.hcw.learn.mybatis.parse;
 
 
 import org.junit.Test;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import javax.xml.xpath.XPathExpressionException;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class XPathParserTest {
 
@@ -12,14 +16,24 @@ public class XPathParserTest {
 
     @Test
     public void testParse() throws FileNotFoundException, XPathExpressionException {
-        String parse = xPathParser.parse("D:\\workspace\\hcw-framework\\hcw-learn-mybatis\\src\\main\\resources\\student.xml","//student[@id='456']");
+        String parse = xPathParser.evalString("D:\\workspace\\hcw-framework\\hcw-learn-mybatis\\src\\main\\resources\\student.xml","//student[@id='456']");
         System.out.println(parse);
     }
 
     @Test
     public void testMapper() throws FileNotFoundException, XPathExpressionException {
-        String parse = xPathParser.parse("D:\\workspace\\hcw-framework\\hcw-learn-mybatis\\src\\main\\resources\\BlogMapper.xml","//select[@id='selectBlogWithPostsUsingSubSelect']");
+        String parse = xPathParser.evalString("D:\\workspace\\hcw-framework\\hcw-learn-mybatis\\src\\main\\resources\\BlogMapper.xml","//select[@id='selectBlogWithPostsUsingSubSelect']");
         System.out.println(parse);
+    }
+    @Test
+    public void testEvalNode() throws IOException, XPathExpressionException, SAXException {
+        Node node =  xPathParser.evalNode("D:\\workspace\\hcw-framework\\hcw-learn-mybatis\\src\\main\\resources\\BlogMapper.xml","/mapper/select");
+        if (node.hasChildNodes()) {
+            NodeList childNodes = node.getChildNodes();
+            for (int i = 0; i < childNodes.getLength(); i++) {
+                System.out.println(childNodes.item(i).getTextContent());
+            }
+        }
     }
 
 }
