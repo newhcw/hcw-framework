bb6edf8178fa6ec142c0bf0047a871f31fe2bfd8
huangchunwu <951123033@qq.com>
2020-08-12 23:39:02 +0800

    add 变种的代理模式

--- hcw-learn-design-pattern/src/main/java/com/hcw/framework/design/pattern/proxy/IntefaceProxy.java
+++ hcw-learn-design-pattern/src/main/java/com/hcw/framework/design/pattern/proxy/IntefaceProxy.java
@@ -5,12 +5,13 @@ import java.lang.reflect.Method;
 
 public class IntefaceProxy implements InvocationHandler {
 
+    // HelloImpl hello; 被代理对象
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         //return method.invoke(this, args);
         //rpc调用
-        System.out.println("invoke");
+        //System.out.println("invoke");
         return null;
     }
 }
