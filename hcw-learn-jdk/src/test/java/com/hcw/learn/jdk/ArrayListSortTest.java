e89c6150d2501ff5fd4d32af375d2ceddbb43e77
huangchunwu <951123033@qq.com>
2020-09-17 18:37:50 +0800

    add  list sort

--- hcw-learn-jdk/src/test/java/com/hcw/learn/jdk/ArrayListSortTest.java
+++ hcw-learn-jdk/src/test/java/com/hcw/learn/jdk/ArrayListSortTest.java
@@ -1,24 +1,22 @@
 package com.hcw.learn.jdk;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class ArrayListSortTest {
 
 
-     List<Integer> list = new ArrayList<>();
+    List<Integer> list = new ArrayList<>();
+    List<User> users = new ArrayList<>();
 
     @BeforeEach
     public void preData() {
         System.out.println("-");
         for (int i = 0; i < 10; i++) {
             list.add(i*2+ new Random().nextInt(100));
+            users.add(new User("xiao ming"+i,+ new Random().nextInt(100)));
         }
     }
 
@@ -34,8 +32,21 @@ public class ArrayListSortTest {
 
     @Test
     public void testLombdaSort() {
-
+        list.sort(Comparator.reverseOrder());//倒序
+        list.forEach(System.out::println);
+        System.out.println("=======================");
+        list.sort(Comparator.naturalOrder());//正序
+        list.forEach(System.out::println);
     }
 
 
+    @Test
+    public void testObjectListSort() {
+        users.sort(Comparator.comparing(User::getAge));// 正序
+        users.forEach(System.out::println);
+        System.out.println("=======================");
+
+        users.sort(Comparator.comparing(User::getAge).reversed());// 倒序
+        users.forEach(System.out::println);
+    }
 }
