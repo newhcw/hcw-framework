2a430832aacdd004919dcadff8f1ff14c6263864
huangchunwu <951123033@qq.com>
2020-11-19 15:21:59 +0800

    spring cloud Hoxton.SR9 spring-boot 2.3.5.RELEASE 可以使用hystrix熔断器

--- hcw-learn-springcloud-consumer/src/main/java/cn/hasfun/learn/springcloud/consumer/HcwLearnSpringcloudConsumerApplication.java
+++ hcw-learn-springcloud-consumer/src/main/java/cn/hasfun/learn/springcloud/consumer/HcwLearnSpringcloudConsumerApplication.java
@@ -1,8 +1,10 @@
 package cn.hasfun.learn.springcloud.consumer;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = {"cn.hasfun"})
@@ -10,6 +12,11 @@ public class HcwLearnSpringcloudConsumerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HcwLearnSpringcloudConsumerApplication.class, args);
+    }
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
     }
 
 }
