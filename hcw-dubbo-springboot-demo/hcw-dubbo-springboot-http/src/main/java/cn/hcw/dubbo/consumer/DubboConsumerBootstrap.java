00f4cb701314ae3f3bf6b6b7915860f84ea07bf5
huangchunwu <951123033@qq.com>
2021-08-04 18:32:41 +0800

    add dubbo

---
+++ hcw-dubbo-springboot-demo/hcw-dubbo-springboot-http/src/main/java/cn/hcw/dubbo/consumer/DubboConsumerBootstrap.java
@@ -0,0 +1,12 @@
+package cn.hcw.dubbo.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboConsumerBootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboConsumerBootstrap.class, args);
+    }
+}
