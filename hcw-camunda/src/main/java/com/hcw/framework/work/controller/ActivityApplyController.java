4a6a2f7cd51da44ed311832b512193cd781ee68f
huangchunwu <951123033@qq.com>
2022-02-15 22:57:22 +0800

    add camunda 用户任务，需要通过流程号查询对应的任务列表，处理

--- hcw-camunda/src/main/java/com/hcw/framework/work/controller/ActivityApplyController.java
+++ hcw-camunda/src/main/java/com/hcw/framework/work/controller/ActivityApplyController.java
@@ -1,14 +1,23 @@
 package com.hcw.framework.work.controller;
 
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.TaskService;
+import org.camunda.bpm.engine.rest.dto.task.TaskDto;
+import org.camunda.bpm.engine.task.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/market/activity")
 public class ActivityApplyController {
@@ -16,6 +25,9 @@ public class ActivityApplyController {
 
     @Autowired
     private RuntimeService runtimeService;
+
+    @Autowired
+    private TaskService taskService;
 
     @RequestMapping(value = "/apply")
     public String apply(@RequestParam(value = "userId")String userId,
@@ -26,5 +38,17 @@ public class ActivityApplyController {
         runtimeService.startProcessInstanceByKey("act_start",variables);
         return "SUCCESS";
     }
+
+    @RequestMapping(value = "/agree")
+    public String agree(@RequestParam(value = "processInstanceId")String processInstanceId){
+        List<Task> taskList = taskService.createTaskQuery().processInstanceId(processInstanceId).list();
+        taskList.stream().forEach(task -> {
+            taskService.complete(taskList.get(0).getId());
+            taskService.createComment(taskList.get(0).getId(), processInstanceId, "提交流程");
+        });
+        return "SUCESS";
+    }
+
+
 
 }
