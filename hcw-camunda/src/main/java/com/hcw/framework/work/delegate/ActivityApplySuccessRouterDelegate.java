12ee919609d2125daa1d4d354b7a586119560987
huangchunwu <951123033@qq.com>
2022-02-21 23:05:52 +0800

    实战-营销活动流程定制-添加各种活动规则

---
+++ hcw-camunda/src/main/java/com/hcw/framework/work/delegate/ActivityApplySuccessRouterDelegate.java
@@ -0,0 +1,27 @@
+package com.hcw.framework.work.delegate;
+
+import com.hcw.framework.work.service.BpmnManagerService;
+import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ActivityApplySuccessRouterDelegate implements JavaDelegate {
+
+    @Autowired
+    private BpmnManagerService bpmnManagerService;
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        String processKey = (String) delegateExecution.getVariable("afterApplyProcessKey");
+        if (bpmnManagerService.isExistProcessDefinitionByKey(processKey)){
+            ProcessInstance processInstance = bpmnManagerService.startProcess(processKey,delegateExecution.getVariables());
+        }else {
+            throw new Exception("未定义流程");
+        }
+    }
+}
